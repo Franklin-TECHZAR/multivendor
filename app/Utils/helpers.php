43674cbc33e1d1bd568eda04aae8ffb1970cb39d
Divakar666 <Dhivakarmm@gmail.com>
2024-05-14 13:19:59 +0530

    pdf compression

--- app/Utils/helpers.php
+++ app/Utils/helpers.php
@@ -977,6 +977,7 @@ class Helpers
         $mpdf_view = $mpdf_view->render();
         
         $mpdf->WriteHTML($mpdf_view);
+        $mpdf->SetCompressionLevel(0);
         $mpdf->Output($file_prefix . $file_postfix . '.pdf', 'D');
     }
 
